db4d2c0353c3afa247e018ab2e95d5a261e8a53a
Kay Edwards <kyedward@travelers.com>
2023-02-02 11:44:12 -0500

    Files added

--- star-wars-app/src/App.js
+++ star-wars-app/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import {Films} from "./components/films"
+import {Planets} from "./components/planets"
+import {People} from "./components/people"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          {/* <Route index element={<Home />} /> */}
+          <Route path="films" element={<Films />} />
+          <Route path="planets" element={<Planets />} />
+          <Route path="people" element={<People />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
