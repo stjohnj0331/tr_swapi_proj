1b5e022ea507c7ccb1be62a771dc124cf3a92386
so and so <soandso@was.com>
2023-02-03 08:27:28 -0500

    added getUrl, updated getPeople

--- star-wars-app/src/components/people.js
+++ star-wars-app/src/components/people.js
@@ -1,35 +1,16 @@
 import React, { useEffect, useState } from "react"; 
-
+import {getUrl} from './getUrl'
 
 export const People = () => {
     const [people, setPeople] = useState([])
     let url = 'https://swapi.dev/api/people';
-/* 
+ 
     useEffect(() => {
-        fetch(`https://swapi.dev/api/films/1`)
-            .then(res = res.json())
-            .then((res) => {
-                setFilm(res);
-            })
-    }, []); */
-    async function getPeople(){
-        const tempPeople = [];
-        while(url){
-            try{
-                const fetchedPeople = await fetch(url)
-                    .then(res => res.json())
-                    .then(res => { url = res.next; return res })
-                    .then(res => res.results)
-                    tempPeople.push(...fetchedPeople)
-            }
-            catch (err){
-                console.error("error reading films" + err.message);
-            }
-        }
-        setPeople(tempPeople);
-    }
+        getUrl(url)
+        .then(listOfPeople => setPeople(listOfPeople))
+    
+    }, [url]); 
 
-    getPeople();
     console.log(people);
     return(
         <>
