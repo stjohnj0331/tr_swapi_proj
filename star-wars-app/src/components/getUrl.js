cfab6aea2ce831f57c06565970a9463afeb25a13
so and so <soandso@was.com>
2023-02-03 08:52:03 -0500

    fixed a staging issue

--- star-wars-app/src/components/getUrl.js
+++ star-wars-app/src/components/getUrl.js
@@ -1,6 +1,7 @@
 export const getUrl = (url) => {
         return fetch(url)
         .then(res => res.json())
+        .then(res => { url = res.next; return res })
         .then(res => {console.log({url});return res})
         .then(res => res.results)
         .catch(err => console.error('Error getting URL', err))
